efb261594074fb3c023f11b51b8499667836d5ff
Ryan Ramage <ryan.ramage@gmail.com>
2017-05-18 14:52:41 -0600

    add a way to copy fields

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
-var urlTemplate = process.argv[2]
-require('../')(process.stdin, urlTemplate).pipe(process.stdout)
+var config = require('rc')('ndjsonurl', {
+  copy: null
+})
+var urlTemplate = config._[0]
+var copy = null
+if (config.copy) {
+  copy = config.copy.split(',')
+}
+require('../')(process.stdin, urlTemplate, copy).pipe(process.stdout)
